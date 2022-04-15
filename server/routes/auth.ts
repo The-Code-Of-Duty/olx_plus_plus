0304d729259a142e0a1a66ffad370ecb258655f4
gaur-abhi524 <abhiabhigaur524@gmail.com>
2022-04-15 23:07:53 +0530

    Add user model

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,7 +2,7 @@ import { Response, Request } from 'express';
 const router = require('express').Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('INSIDE homepage');
+    
 });
 
 export default router;
