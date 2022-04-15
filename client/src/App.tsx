2bd82aebaaec9d1e024695ac09bb58e60af553c3
akshatnema <20bcs022@iiitdmj.ac.in>
2022-04-15 20:19:58 +0530

    all objects rendered
    Signed-off-by: akshatnema <20bcs022@iiitdmj.ac.in>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 // import './App.css'
+import Navbar from "./components/Navbar/Navbar"
+import Category from "./components/Category/Category"
 import GoogleLoginComp from "./components/Google/googleLogin";
 import Footer from "./components/Footer/footer";
 import PostCard from "./components/Post/post";
@@ -11,6 +13,8 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
+      <Category />
       <PostGrid />
       <Footer />
     </div>
--- client/src/components/PostGrid/postgrid.tsx
+++ client/src/components/PostGrid/postgrid.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Box, { BoxProps } from "@mui/material/Box";
 import PostCard from "../Post/post";
+import Container from '@mui/material/Container';
 import "./Postgrid.scss";
 
 export default function PostGrid() {
   return (
     <div style={{ width: "100%" }}>
+      <Container>
       <div className="post">
         <div className="post-card">
           <PostCard />
@@ -23,6 +25,7 @@ export default function PostGrid() {
           <PostCard />
         </div>
       </div>
+      </Container>
     </div>
   );
 }
