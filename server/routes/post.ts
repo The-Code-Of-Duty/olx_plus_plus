2e2044d282052a18b37c7fe67fbeb3d0152b26b6
Manav Desai <manav27202@gmail.com>
2022-04-16 15:09:55 +0530

    Added: Dynamic rendering PostDetails

--- server/routes/post.ts
+++ server/routes/post.ts
@@ -1,43 +1,44 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 const router = require('express').Router();
 const Post = require('../models/post')
 
-router.post('/post', async (req:Request, res:Response) => {
-      const newPost = new Post(req.body);
-      console.log(newPost)
-      try {
-        const savepost = await newPost.save();
-        res.status(200).json(savepost);
-      } catch (err) {
-         console.log(err);
-        res.status(500).send(err);
-      }
+router.post('/post', async (req: Request, res: Response) => {
+  const newPost = new Post(req.body);
+  console.log(newPost)
+  try {
+    const savepost = await newPost.save();
+    res.status(200).json(savepost);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 })
 
-router.get('/', async (req:Request, res:Response) => {
-    const Posts = await Post.find({});
-    console.log(Posts);
-    try{
-        res.status(200).json(Posts);
-    }catch(err){
-        console.log(err);
-    }
+router.get('/', async (req: Request, res: Response) => {
+  const Posts = await Post.find({});
+  console.log(Posts);
+  try {
+    res.status(200).json(Posts);
+  } catch (err) {
+    console.log(err);
+  }
 })
 
-router.get('/post:id' ,async (req:Request, res:Response) => {
-    const id = String(req.params.id);
-    try{
-      const PostData = await Post.findOne({
-        _id: id
-      })
-      console.log(PostData);
-      res.send(200).json(PostData);
-    }
-    catch(err){
-      console.log(err);
-    }
-    
-    
+router.get('/post/:id', async (req: Request, res: Response) => {
+  const id = String(req.params.id);
+  try {
+    const PostData = await Post.findOne({
+      _id: id
+    })
+    console.log("PostData", PostData);
+
+    res.json(PostData);
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+
 })
 
 export default router;
